0b02adcd1048175ab894218883e76da845fceb76
Vikas Babu <23bma048@nith.ac.in>
2026-03-23 00:30:05 +0530

    backend url added

--- src/components/admin/protectedRoute.jsx
+++ src/components/admin/protectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useSelector((store) => store.auth);
-
+  console.log(user)
   if (!user || user.role !== "recruiter") {
     return <Navigate to="/" replace />;
   }
